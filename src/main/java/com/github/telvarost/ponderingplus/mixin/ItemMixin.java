ee7c9ad18ee5ac29b6dea596947acf7a4842ffff
telvarost <telontelvarost@gmail.com>
2025-03-04 23:45:10 -0500

    Storybookshelf also costs a paper

--- src/main/java/com/github/telvarost/ponderingplus/mixin/ItemMixin.java
+++ src/main/java/com/github/telvarost/ponderingplus/mixin/ItemMixin.java
@@ -118,6 +118,7 @@ public class ItemMixin {
                         player.openEditSignScreen(signBlockEntity);
                     }
 
+                    itemStack.count--;
                     cir.setReturnValue(true);
                 }
             }
